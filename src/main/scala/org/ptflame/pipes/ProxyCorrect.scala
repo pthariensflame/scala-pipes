9bfe276c15959247e2797948dd0f90b11374352b
Alexander Altman <alexanderaltman@me.com>
2013-01-03 13:56:43 -0800

    Refactor
    [none]

--- src/main/scala/org/ptflame/pipes/ProxyCorrect.scala
+++
@@ -1,8 +0,0 @@
-package org.ptflame.pipes
-package internal
-
-sealed abstract class ProxyF[+Uo, -Ui, -Di, +Do, +M[+_], +A] {
-
-  
-
-}
--- src/main/scala/org/ptflame/pipes/package.scala
+++ src/main/scala/org/ptflame/pipes/package.scala
@@ -1,8 +1,5 @@
 package org.ptflame
 
 package object pipes {
-  import scalaz.Free
-
-  type ProxyCorrect[+Uo, -Ui, -Di, +Do, +M[+_], +A] = Free[({ type f[+r] = ProxyF[] })#f]
 
 }
