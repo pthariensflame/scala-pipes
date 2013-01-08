89b5a403fa9a2a283aa819d9e5e19361f81569da
Alexander Altman <alexanderaltman@me.com>
2013-01-08 15:06:41 -0800

    Some testing work
    [none]

--- build.sbt
+++ build.sbt
@@ -2,12 +2,12 @@ name                :=  "scala-pipes"
 
 version             :=  "1.0.0-SNAPSHOT"
 
-crossScalaVersions  :=  Seq("2.9.2", "2.10.0")
+scalaVersion        :=  "2.10.0"
 
 libraryDependencies ++= Seq(
   "org.scalaz"     %% "scalaz-core"        % "7.0.0-M7"
   ,
-  "org.scalaz"     %% "scalaz-scalacheck-binding" % "7.0.0-M7" % "test"
+  "org.scalaz"     %% "scalaz-scalacheck-binding" % "7.0.0-M7"
   ,
-  "org.scalacheck" %% "scalacheck"         % "1.10.0"          % "test"
+  "org.scalacheck" %% "scalacheck"         % "1.10.0"
 )
--- src/main/scala/org/ptflame/pipes/scalacheck/package.scala
+++ src/main/scala/org/ptflame/pipes/scalacheck/package.scala
@@ -1,14 +1,44 @@
 package org.ptflame.pipes
-import org.scalatools.testing.Framework
+import scalaz.{Monad, MonadPlus}, org.scalacheck.{Properties, Arbitrary, Gen}, org.scalacheck.Prop.forAll, scalaz.scalacheck.ScalazProperties.{monad, monadPlus}
 
 package object scalacheck {
 
-  def 
+  def proxyProperties[P[+_, -_, -_, +_, +_]](Px: Proxy[P]): Properties = new Properties("proxy") with ProxyProperties[P] {
+
+    implicit override val P: Proxy[P] = Px
+
+  }
+
+  def proxyPlusProperties[P[+_, -_, -_, +_, +_]](Px: ProxyPlus[P]): Properties = new Properties("proxy") with ProxyPlusProperties[P] {
+
+    implicit override val P: ProxyPlus[P] = Px
+
+  }
+
+  implicit def proxyArbitrary[P[+_, -_, -_, +_, +_], Uo, Ui, Di, Do, A] = Arbitrary()
 
 }
 
 package scalacheck {
 
-  
+  private[scalacheck] trait ProxyProperties[P[+_, -_, -_, +_, +_]] { this: Properties =>
+
+    implicit val P: Proxy[P]
+
+    implicit val PM: Monad[({ type f[+a] = P[Byte, Short, Long, Char, a] })#f] = P.monad[Byte, Short, Long, Char]
+    
+    property("monad") = monad.laws[({ type f[+a] = P[Byte, Short, Long, Char, a] })#f]
+
+  }
+
+  private[scalacheck] trait ProxyPlusProperties[P[+_, -_, -_, +_, +_]] extends ProxyProperties[P] { this: Properties =>
+
+    implicit override val P: ProxyPlus[P]
+
+    implicit override val PM: MonadPlus[({ type f[+a] = P[Byte, Short, Long, Char, a] })#f] = P.monad[Byte, Short, Long, Char]
+
+    property("monadPlus") = monadPlus.laws[({ type f[+a] = P[Byte, Short, Long, Char, a] })#f]
+
+  }
 
 }
