5ea4e45fdc452bd0e1e5ec9202897659117c4942
Alexander Altman <alexanderaltman@me.com>
2013-01-10 15:15:05 -0800

    doc fix
    [none]

--- src/main/scala/org/ptflame/pipes/syntax/package.scala
+++ src/main/scala/org/ptflame/pipes/syntax/package.scala
@@ -4,7 +4,7 @@ import scalaz.syntax.Ops
 
 /**
  * Syntax for proxies.
- */s
+ */
 package object syntax {
 
   implicit class ProxyKOps[I, P[+_, -_, -_, +_, +_], Uo, Ui, Di, Do, A](override val self: I => P[Uo, Ui, Di, Do, A]) extends Ops[I => P[Uo, Ui, Di, Do, A]] {
