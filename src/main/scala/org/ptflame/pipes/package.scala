354f8c0d54e4e2fdf04c109038d7b7d8a9134857
Alexander Altman <alexanderaltman@me.com>
2013-01-10 16:57:22 -0800

    Some additions
    [none]

--- src/main/scala/org/ptflame/pipes/package.scala
+++ src/main/scala/org/ptflame/pipes/package.scala
@@ -15,4 +15,24 @@ package object pipes extends IdInstances with ProxyBaseTInstances {
 
   object ProxyBase extends ProxyBaseTInstances
 
+  object lib {
+
+    def idP[P[+_, -_, -_, +_, +_], U, D, A](a: => U)(implicit P: Proxy[P]): P[U, D, U, D, A] = {
+      val PM: Monad[({ type f[+a] = P[U, D, U, D, a] })#f] = P.monad[U, D, U, D]
+      def go(x: => U): P[U, D, U, D, A] = PM.bind(PM.bind(P.request(x))(P.respondK)) { go(_) }
+      go(a)
+    }
+
+    @inline def idK[P[+_, -_, -_, +_, +_], U, D, A](implicit P: Proxy[P]): U => P[U, D, U, D, A] = { idP[P, U, D, A](_) }
+
+    def coidP[P[+_, -_, -_, +_, +_], U, D, A](a: => D)(implicit P: Proxy[P]): P[U, D, U, D, A] = {
+      val PM: Monad[({ type f[+a] = P[U, D, U, D, a] })#f] = P.monad[U, D, U, D]
+      def go(x: => D): P[U, D, U, D, A] = PM.bind(PM.bind(P.respond(x))(P.requestK)) { go(_) }
+      go(a)
+    }
+
+    @inline def coidK[P[+_, -_, -_, +_, +_], U, D, A](implicit P: Proxy[P]): D => P[U, D, U, D, A] = { coidP[P, U, D, A](_) }
+
+  }
+
 }
