0973a811ed2a78fa739996d1e7d7952c5d19915a
Alexander Altman <alexanderaltman@me.com>
2013-02-19 20:42:49 -0800

    Added `IdentityP` and the `trans` package
    [none]

---
+++ src/main/scala/org/ptflame/pipes/trans/IdentityP.scala
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.ptflame.pipes
+package trans
+
+final case class IdentityP[P[+_, -_, -_, +_, +_], +Uo, -Ui, -Di, +Do, +A](run: P[Uo, Ui, Di, Do, A]) {
+
+  def map[Uo1 >: Uo, Ui1 <: Ui, Di1 <: Di, Do1 >: Do, B](f: A => B)(implicit P: Proxy[P]): IdentityP[P, Uo1, Ui1, Di1, Do1, B] = IdentityP(P.monad[Uo1, Ui1, Di1, Do1].map[A, B](this.run)(f))
+
+  def ap[Uo1 >: Uo, Ui1 <: Ui, Di1 <: Di, Do1 >: Do, B](f: IdentityP[P, Uo1, Ui1, Di1, Do1, (A => B)])(implicit P: Proxy[P]): IdentityP[P, Uo1, Ui1, Di1, Do1, B] = IdentityP(P.monad[Uo1, Ui1, Di1, Do1].ap[A, B](this.run)(f.run))
+
+  def flatMap[Uo1 >: Uo, Ui1 <: Ui, Di1 <: Di, Do1 >: Do, B](f: A => IdentityP[P, Uo1, Ui1, Di1, Do1, B])(implicit P: Proxy[P]): IdentityP[P, Uo1, Ui1, Di1, Do1, B] = IdentityP(P.monad[Uo1, Ui1, Di1, Do1].bind[A, B](this.run)(f andThen { _.run }))
+
+  @inline def flatten[Uo1 >: Uo, Ui1 <: Ui, Di1 <: Di, Do1 >: Do, B](implicit P: Proxy[P], ev: A <:< IdentityP[P, Uo1, Ui1, Di1, Do1, B]): IdentityP[P, Uo1, Ui1, Di1, Do1, B] = this.flatMap[Uo1, Ui1, Di1, Do1, B](ev)(P)
+
+}
+
+object IdentityP extends IdentityPInstances
+
+private[trans] trait IdentityPInstances {
+
+  
+
+}
---
+++ src/main/scala/org/ptflame/pipes/trans/package.scala
@@ -0,0 +1,3 @@
+package org.ptflame.pipes
+
+package object trans extends IdentityPInstances
